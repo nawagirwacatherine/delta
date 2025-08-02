37a81c5df0ec0a27561130e384146620c06fecda
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-08-02 11:42:25 +0200

    added designs to all the navbars for different pages

--- src/components/ClinicFinderNav.jsx
+++ src/components/ClinicFinderNav.jsx
@@ -1,30 +1,35 @@
-
 import React from 'react'
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import { GiBlood } from 'react-icons/gi';
-import { GiWomanElfFace } from 'react-icons/gi';
+import { GiBlood, GiWomanElfFace, GiSextant } from 'react-icons/gi';
 import { RiMentalHealthFill } from 'react-icons/ri';
-import { GiSextant } from 'react-icons/gi';
-
 
 const ClinicFinderNav = () => {
-    
   return (
     <>
- <div className=' flex gap-10 align-middle justify-center p-5 '>
-  
-       
-
-
+      <div className='flex gap-10 align-middle justify-center p-5'>
         <ul className='flex gap-3.5'>
-                  <li className=' p-6' style={{ borderRadius:'50%'}}><AnchorLink className=''  href='#disease'><p> <GiBlood className=' '/>GPSmap  </p></AnchorLink></li>
-                          <li className='p-6 '  style={{ borderRadius:'50%'}}><AnchorLink className='' offset={50} href='#symptoms'><p> <GiWomanElfFace/>Contact buttons</p></AnchorLink></li>
-                                  <li className=' p-6 '  style={{ borderRadius:'50%'}}><AnchorLink className='' offset={50} href='#challenges'><p> <RiMentalHealthFill/>Emergency Info</p></AnchorLink></li>
-                                          <li className='p-6'  style={{ borderRadius:'50%'}}><AnchorLink className='' offset={50} href='#details'><p> <GiSextant/> Suggestion If any</p></AnchorLink></li>
+          <li className='p-6' style={{ borderRadius: '5px', background: '#4ABBB7', color: 'white' }}>
+            <AnchorLink href='#disease'>
+              <p><GiBlood /> GPS Map</p>
+            </AnchorLink>
+          </li>
+          <li className='p-6' style={{ borderRadius: '5px', background: '#4ABBB7', color: 'white' }}>
+            <AnchorLink offset={50} href='#symptoms'>
+              <p><GiWomanElfFace /> Contact Buttons</p>
+            </AnchorLink>
+          </li>
+          <li className='p-6' style={{ borderRadius: '5px', background: '#4ABBB7', color: 'white' }}>
+            <AnchorLink offset={50} href='#challenges'>
+              <p><RiMentalHealthFill /> Emergency Info</p>
+            </AnchorLink>
+          </li>
+          <li className='p-6' style={{ borderRadius: '5px', background: '#4ABBB7', color: 'white' }}>
+            <AnchorLink offset={50} href='#details'>
+              <p><GiSextant /> Suggestions (if any)</p>
+            </AnchorLink>
+          </li>
         </ul>
-
-    </div>
-
+      </div>
     </>
   )
 }
--- src/components/TopicsPageNav.jsx
+++ src/components/TopicsPageNav.jsx
@@ -1,26 +1,31 @@
-
 import React from 'react'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { GiBlood } from 'react-icons/gi';
 import { RiMentalHealthFill } from 'react-icons/ri';
 import { GiSextant } from 'react-icons/gi';
 
-
 const TopicsPageNav = () => {
-    
   return (
     <>
- <div className=' flex gap-10 align-middle justify-center p-5 ' >
-
+      <div className='flex gap-10 align-middle justify-center p-5'>
         <ul className='flex gap-3.5'>
-                  <li className=' p-6' style={{ borderRadius:'50%'}}><AnchorLink className=''  href='#disease'><p> <GiBlood className=' '/>Weekly tips</p></AnchorLink></li>
-                                  <li className=' p-6 '  style={{ borderRadius:'50%'}}><AnchorLink className='' offset={50} href='#challenges'><p> <RiMentalHealthFill/>Step-by-step visuals or illustrations</p></AnchorLink></li>
-                                          <li className='p-6'  style={{ borderRadius:'50%'}}><AnchorLink className='' offset={50} href='#details'><p> <GiSextant/>"Ask a Question" button,links to anonymous Chatbot</p></AnchorLink></li>
+          <li className='p-6' style={{ borderRadius: '5px', background: '#4ABBB7', color: 'white' }}>
+            <AnchorLink href='#disease'>
+              <p><GiBlood /> Weekly tips</p>
+            </AnchorLink>
+          </li>
+          <li className='p-6' style={{ borderRadius: '5px', background: '#4ABBB7', color: 'white' }}>
+            <AnchorLink offset={50} href='#challenges'>
+              <p><RiMentalHealthFill /> Step-by-step visuals or illustrations</p>
+            </AnchorLink>
+          </li>
+          <li className='p-6' style={{ borderRadius: '5px', background: '#4ABBB7', color: 'white' }}>
+            <AnchorLink offset={50} href='#details'>
+              <p><GiSextant /> "Ask a Question" button, links to anonymous Chatbot</p>
+            </AnchorLink>
+          </li>
         </ul>
-         </div>
-
-   
-
+      </div>
     </>
   )
 }
